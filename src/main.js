ec294c43621e81ca255c458ece00771d0b2c8cc5
wolneyljklin-prog <wolneyljklin@gmail.com>
2026-05-28 09:47:17 -0300

    Refactor main.js to enhance app structure and interactivity

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
-document.getElementById('app').innerHTML = `
-  <h1>Meu primeiro app</h1>
-  <p>Agora sim está funcionando de verdade 🚀</p>
+const app = document.getElementById('app')
+
+app.innerHTML = `
+  <h1>Meu App 🚀</h1>
+  <button id="btn">Clique aqui</button>
+  <p id="texto"></p>
 `
+
+document.getElementById('btn').addEventListener('click', () => {
+  document.getElementById('texto').innerText = 'Você clicou no botão ✅'
+})
 
 
 
