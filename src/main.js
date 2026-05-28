96f7fde27daf163ef8d7a146da18209a294534a2
wolneyljklin-prog <wolneyljklin@gmail.com>
2026-05-28 09:39:42 -0300

    Update app content in main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
-const app = document.getElementById("app");
-
-app.innerHTML = `
-  <h1>Funcionando 🚀</h1>
-`;document.getElementById('app').innerHTML = `
+document.getElementById('app').innerHTML = `
   <h1>Meu primeiro app</h1>
   <p>Agora sim está funcionando de verdade 🚀</p>
 `
+
 
 
