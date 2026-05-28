68688902497ca881aec5ba4fc286ec76a65f8183
wolneyljklin-prog <wolneyljklin@gmail.com>
2026-05-28 09:37:26 -0300

    Update app content with new message

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ const app = document.getElementById("app");
 
 app.innerHTML = `
   <h1>Funcionando 🚀</h1>
-`;
+`;document.getElementById('app').innerHTML = `
+  <h1>Meu primeiro app</h1>
+  <p>Agora sim está funcionando de verdade 🚀</p>
+`
+
 
